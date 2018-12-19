80087ffe7a071d1c35db5200b93562c3aa95b3fd
Gal <galnik.kozmelj@gmail.com>
2018-12-19 08:56:54 +0100

    badges

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,29 @@ import React, { Component } from "react";
 //counter component
 
 class Counter extends Component {
-  state = { count: 0 };
+  state = {
+    count: 0
+  };
 
   render() {
+    let classes = this.getBadgeClasses();
+
     return (
       <React.Fragment>
-        <span>Hello World</span>
-        <button>increment</button>
+        <span className={classes}>{this.formatCount()}</span>
+        <button className="btn-secondary btn-sm">increment</button>
       </React.Fragment>
     );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : count;
   }
 }
 
