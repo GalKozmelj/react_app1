0f47108d8da176aacb3d683cf939a6fde34da2af
Gal <galnik.kozmelj@gmail.com>
2018-12-19 09:16:37 +0100

    edited tags

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,28 +4,23 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
 
-  render() {
-    let classes = this.getBadgeClasses();
-
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
     return (
-      <React.Fragment>
-        <span className={classes}>{this.formatCount()}</span>
-        <button className="btn-secondary btn-sm">increment</button>
-      </React.Fragment>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}> {tag} </li>
+        ))}
+      </ul>
     );
   }
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : count;
+  render() {
+    return <div>{this.renderTags()}</div>;
   }
 }
 
