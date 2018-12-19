3dcf225bf7f7864d3c59357c9d48be71329bec42
Gal <galnik.kozmelj@gmail.com>
2018-12-19 09:24:49 +0100

    onclick event for button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,23 +4,28 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    count: 0
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+  render() {
+    let classes = this.getBadgeClasses();
+
     return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}> {tag} </li>
-        ))}
-      </ul>
+      <React.Fragment>
+        <span className={classes}>{this.formatCount()}</span>
+        <button className="btn-secondary btn-sm">increment</button>
+      </React.Fragment>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-  render() {
-    return <div>{this.renderTags()}</div>;
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : count;
   }
 }
 
